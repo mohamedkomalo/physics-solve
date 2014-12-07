3b75b6b090b10c755069724ab271faff1a17a36b
Mohamed Kamal Kamaly <mohamedkomalo@hotmail.com>
2014-12-07 12:59:57 +0200

    added force meter with up and down to increase and decerase force

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 (function(){
 	var GAME_SIZE = {width:700, height: 500};
-
+	var GAME_SCALE = 10;
 
 	var canvas = document.getElementById('game');
 	//var context = canvas.getContext('2d');
@@ -9,13 +9,13 @@
 	canvas.height = GAME_SIZE.height;
 
 	var world = boxbox.createWorld(canvas, {
-		scale:10,
+		scale: GAME_SCALE,
 		gravity: {x:0, y:9.8}
 	});
 
 	var boundaryTemplate = {
 		name: "boundary",
-		shapre: "square",
+		shape: "square",
 		color: "lightblue",
 		borderColor: "transparent",
 		type: "static",
@@ -34,7 +34,7 @@
 	var renderEntityPositionFunction = function(context){
 		var pos = this.position();
 		//console.log(pos);
-		context.fillText("(" + parseInt(pos.x) + "," + parseInt(pos.y) + ")", pos.x * 10, pos.y * 10);
+		context.fillText("(" + parseInt(pos.x) + "," + parseInt(pos.y) + ")", pos.x * GAME_SCALE, pos.y * GAME_SCALE);
 	}
 
 	var ballon = world.createEntity({
@@ -42,11 +42,12 @@
 		shape: "circle", 
 		radius: .3,
 		density: 2,
-		x:10,
-		y:5,
+		x: 2,
+		y: 5,
+		$fellOff: false,
 		onRender: renderEntityPositionFunction,
 		onStartContact: function(entity){
-			if(entity.name() == "boundary"){
+			if(entity.name() == "boundary" && this.$fellOff === false){
 				this.clearVelocity("moving");
 				BALLOON_DIRECTION *= -1;
 				this.setVelocity( "moving", BALLOON_SPEED, BALLOON_DIRECTION);
@@ -56,21 +57,69 @@
 			if(entity.name() == "nail"){
 				this.clearVelocity("moving");
 				this.clearForce("moving");
+				this.$fellOff = true;
 			}
 		}
 	});
 
 	var upthrustForce = (2 * (22 / 7) * (.3 * .3)) * 9.8;
 	ballon.setForce( "moving", upthrustForce, 0 );
-	 ballon.setVelocity( "moving", BALLOON_SPEED, BALLOON_DIRECTION);
+	ballon.setVelocity( "moving", BALLOON_SPEED, BALLOON_DIRECTION);
 	
+	var MAX_FORCE = 100;
+
+	var thrower = world.createEntity({
+		name: "thrower",
+		shape: "square",
+		color: "black",
+		width: 2,
+		height: 7,
+		x: 30,
+		y: 48,
+		$power:60,
+		onKeydown: function(event){
+			console.log(event);
+			if(this.$power < 100){
+				if(event.keyCode === 38){ //UP
+					this.$power += 1;
+				}
+			}
+			
+			if(this.$power > 1){
+				if(event.keyCode === 40){ //DOWN
+					this.$power -= 1;
+				}
+			}
+		},
+		onRender: function(context){
+			var entityPos = this.position();
+			var entitySize = {width: 2, height: 7};
+
+			var x = entityPos.x - entitySize.width / 2 + .3;
+			var y = entityPos.y - entitySize.height / 2 + .3;
+			var width = entitySize.width - .6;
+			var height = entitySize.height - .6;
+
+			//var size = {width: this.width(), height: this.height()};
+			// console.log(size);
+
+			context.fillStyle = "grey";
+			context.fillRect(x * GAME_SCALE, (y + (1-this.$power/MAX_FORCE) * height) * GAME_SCALE, width * GAME_SCALE, (this.$power/MAX_FORCE) * height * GAME_SCALE);
+
+			var powerTextX = x + width + 1;
+			var powerTextY = y + height/2;
+
+			context.fillText(this.$power + "N", powerTextX * GAME_SCALE, powerTextY * GAME_SCALE);
+		}
+	});
+
 	var nail = world.createEntity({
 		name: "nail", 
 		shape: "circle", 
 		radius: .3,
 		density: 2,
-		y:30,
 		x:30,
+		y:41,
 		$thrown: false,
 		onRender: renderEntityPositionFunction,
 		onKeydown: function(event){
