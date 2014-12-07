f26261d2a3da2f53f1e24dee6eb4a29a31b6b4c4
Mohamed Kamal Kamaly <mohamedkomalo@hotmail.com>
2014-12-07 05:08:04 +0200

    initial canvas config

---
+++ game.js
@@ -0,0 +1,9 @@
+(function(){
+	var GAME_SIZE = {width:500, height: 500};
+
+
+	var canvas = document.getElementById('game');
+	canvas.width = GAME_SIZE.width;
+	canvas.height = GAME_SIZE.height;
+
+})()
