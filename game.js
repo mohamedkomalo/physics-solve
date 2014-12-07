4063bd7e570ed054a57d9c542caab925af117bf9
Mohamed Kamal Kamaly <mohamedkomalo@hotmail.com>
2014-12-07 11:01:02 +0200

    added boundaries, pressing a key will throw the down ball one time only

--- game.js
+++ game.js
@@ -3,75 +3,67 @@
 
 
 	var canvas = document.getElementById('game');
+
 	canvas.width = GAME_SIZE.width;
 	canvas.height = GAME_SIZE.height;
 
-	var world = boxbox.createWorld(canvas, {scale:10,
-		gravity: {x:0, y:9.8}});
+	var world = boxbox.createWorld(canvas, {
+		scale:10,
+		gravity: {x:0, y:9.8}
+	});
 
-
-	var ground = world.createEntity({
-		name: "ground",
+	var boundaryTemplate = {
+		name: "boundary",
 		shapre: "square",
-		color: "red",
+		color: "lightblue",
+		borderColor: "transparent",
 		type: "static",
-		width: GAME_SIZE.width,
-		height:2,
-		y: 30,
-	})
+		width:GAME_SIZE.width,
+		height:GAME_SIZE.height
+	};
 
-	var ground = world.createEntity({
-		name: "ground",
-		shapre: "square",
-		color: "red",
-		type: "static",
-		width: 2,
-		height:GAME_SIZE.height,
-		x: 48
-	})
+	var leftBoundary = world.createEntity(boundaryTemplate, { width: 2, x: 1 });
+	var rightBoundary = world.createEntity(boundaryTemplate, { width: 2, x: 69 });
+	var bottomBoundary = world.createEntity(boundaryTemplate, { height: 2, y: 49 });
 
 	var ballon = world.createEntity({
 		name: "ballon", 
 		shape: "circle", 
-		radius: 1,
+		radius: .3,
 		density: 2,
-		onTick:function(){
-			//console.log(ballon.position());
-			//world.createJoint(ballon, ground, {type:"distance"});
-		},
+		x:10,
+		y:5,
 		onImpact:function(entity){
 			if(entity.name() == "nail"){
 				this.clearVelocity("moving");
 				this.clearForce("moving");
 				console.log('done');
 			}
-		},
-		x:10,
-		y:5
+		}
 	});
+
+	var upthrustForce = (2 * (22 / 7) * (.1 * .1)) * 9.8;
+	ballon.setForce( "moving", upthrustForce, 0 );
+	ballon.setVelocity( "moving", 20, 90 );
 	
 	var nail = world.createEntity({
 		name: "nail", 
 		shape: "circle", 
-		radius: 1,
+		radius: .3,
 		density: 2,
-		onTick:function(){
-			console.log(ballon.position());
-			//world.createJoint(ballon, ground, {type:"distance"});
-		},
-		onRender: function(){
-
-		},
 		y:30,
-		x:30
+		x:30,
+		$thrown: false,
+		onKeydown: function(event){
+			if(this.$thrown === false){
+				this.applyImpulse(10, 0);
+				this.$thrown = true;
+			}
+		}
 	});
 
 	//nail.applyImpulse(132, 0);
-	var upthrustForce = (2 * (22 / 7) * (1 * 1)) * 9.8;
-	console.log(upthrustForce);
-	ballon.setForce( "moving", upthrustForce, 0 );
-	ballon.setVelocity( "moving", 10, 90 );
 	//nail.setVelocity("to kill", 12.5, 0);
-	nail.applyImpulse(141.3, 0);
+	//nail.applyImpulse(141.3, 0);
 
 })()
